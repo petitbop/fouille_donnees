f32584c70c6ced4d7d9733b03bb347413e8903b9
Florian Privé <florian.prive@ensimag.grenble-inp.fr>
2015-03-03 17:29:55 +0100

    avancee

--- tp1/useful-commands.R
+++ tp1/useful-commands.R
@@ -28,3 +28,33 @@ X <- mylm$residuals
 sum(X*X)
 # 41.81094
 
+
+4)a)
+# overfitting, plus on rajoute de données, plus on fit les points mais ce n'est pas satisfaisant car on prédit mal de nouvelles données
+lm(lpsa~1,data=pro) # approxime par la moyenne
+
+lm(lpsa~.,data=pro[,c(1,4,9)]) # équivalent à lm(lpsa~lcavol + lbph,data=pro) => approxime en fonction de lcavol et lbph 
+
+lm(lpsa~.,data=pro[,c(2,7,9)]) # équivalent à lm(lpsa~lweight + gleason,data=pro) => approxime en fonction de lweigth et gleason
+
+X <- mylm$residuals
+sum(X*X)
+
+A <- combn(8, 2)
+for(i in 1:length(A[1,])) {
+    mylm <- lm(lpsa~., data = pro[,c(A[1,i], A[2,i], 9)])
+    res <- mylm$residuals
+    print(sum(res*res))
+}
+
+5)a) 
+# garder des donnees pour valider le modele (pour eviter l'overfitting)
+
+5)b)
+valid <- seq(from = 1, to = length(pro[,1]), by = 2) 
+
+5)c)
+lm(lpsa~.,data=pro[-valid,c(i,j,9)]) # utilise les colonnes i et j pour predire lpsa en retirant les lignes dont les indices donc dans valid 
+RSS
+
+5)d)
