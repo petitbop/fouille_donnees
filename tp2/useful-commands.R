03f31ee10b08ca25d1e88222500c244271c04149
Florian Privé <florian.prive@ensimag.grenble-inp.fr>
2015-03-16 18:18:04 +0100

    trois

--- tp2/useful-commands.R
+++ tp2/useful-commands.R
@@ -23,3 +23,15 @@ library(maps);map("world",add=T)
 NAaux <- NAm2[,-(1:7)]
 mylm <- lm(formula = long ~., data = NAaux)
 summary(mylm)
+
+# 3.b)
+NAgen <- NAm2[,-(1:8)]
+pcaNAm2 <- prcomp(NAgen)
+
+caxes=c(1,2)
+plot(pcaNAm2$x[,caxes],col="white")
+for (i in 1:npop) {
+print(names[i])
+lines(pcaNAm2$x[which(NAm2[,3]==names[i]),caxes],type="p",col=colPalette[i],pch=pch[i])
+}
+legend("top",legend=names,col=colPalette,lty=-1,pch=pch,cex=.75,ncol=3,lwd=2)
