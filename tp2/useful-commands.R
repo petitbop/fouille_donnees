a9aa8d29d524ee634b84ccabe651bdf58096c287
Pierre Petitbon <pierre.petitbon@ensimag.grenoble-inp.fr>
2015-03-16 17:44:18 +0100

    correction erreur plot

--- tp2/useful-commands.R
+++ tp2/useful-commands.R
@@ -9,8 +9,8 @@ names(NAm2[,1:10])
 names=unique(NAm2$Pop)
 npop=length(names)
 coord=unique(NAm2[,c("Pop","long","lat")]) #coordinates for each pop
-colPalette=rep(c("black","red","cyan","orange","brown","blue","pink","purple","
-darkgreen"),3)
+colPalette=rep(c("black","red","cyan","orange","brown","blue","pink","purple",
+"darkgreen"),3)
 pch=rep(c(16,15,25),each=9)
 plot(coord[,c("long","lat")],pch=pch,col=colPalette,asp=1)
 # asp permet d'avoir le rapport correct entre les axes longitude et latitude
