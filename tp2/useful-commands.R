fdf9cd554d06b68090ca827421cef29d1f7d130b
Florian Privé <florian.prive@ensimag.grenble-inp.fr>
2015-03-16 18:29:45 +0100

    trois

--- tp2/useful-commands.R
+++ tp2/useful-commands.R
@@ -28,12 +28,30 @@ summary(mylm)
 
 # 3.b)
 NAgen <- NAm2[,-(1:8)]
-pcaNAm2 <- prcomp(NAgen)
+pcaNAm2 <- prcomp(NAgen) # normalisation intrinsèque
 
+# ACP avec axes 1 et 2
 caxes=c(1,2)
 plot(pcaNAm2$x[,caxes],col="white")
 for (i in 1:npop) {
 print(names[i])
 lines(pcaNAm2$x[which(NAm2[,3]==names[i]),caxes],type="p",col=colPalette[i],pch=pch[i])
 }
+legend("top",legend=names,col=colPalette,lty=-1,pch=pch,cex=.75,ncol=3,lwd=2)
+
+# ACP avec 5 et 6
+caxes=c(5,6)
+plot(pcaNAm2$x[,caxes],col="white")
+for (i in 1:npop) {
+print(names[i])
+lines(pcaNAm2$x[which(NAm2[,3]==names[i]),caxes],type="p",col=colPalette[i],pch=pch[i])
+}
+legend("top",legend=names,col=colPalette,lty=-1,pch=pch,cex=.75,ncol=3,lwd=2)
+
+# ACP juste pour un 
+caxes=c(1,2)
+plot(pcaNAm2$x[,caxes],col="white")
+i = 3 # i peut aller de 1 à npop=27
+print(names[i])
+lines(pcaNAm2$x[which(NAm2[,3]==names[i]),caxes],type="p",col=colPalette[i],pch=pch[i])
 legend("top",legend=names,col=colPalette,lty=-1,pch=pch,cex=.75,ncol=3,lwd=2)
