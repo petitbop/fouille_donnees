c5956c28860751f25af7a2270f066ee5d14e2fe2
Pierre Petitbon <Pierre.Petitbon@ensimag.grenoble-inp.fr>
2015-03-26 17:37:54 +0100

    modifs mineures

--- tp2/useful-commands.R
+++ tp2/useful-commands.R
@@ -12,11 +12,13 @@ npop=length(names)
 coord=unique(NAm2[,c("Pop","long","lat")]) #coordinates for each pop
 colPalette=rep(c("black","red","cyan","orange","brown","blue","pink","purple","darkgreen"),3)
 pch=rep(c(16,15,25),each=9)
+pdf("map.pdf")
 plot(coord[,c("long","lat")],pch=pch,col=colPalette,asp=1)
 # asp permet d'avoir le rapport correct entre les axes longitude et latitude
 # ainsi la carte n'est pas déformée
 legend("bottomleft",legend=names,col=colPalette,lty=-1,pch=pch,cex=.75,ncol=2,lwd=2)
 library(maps);map("world",add=T)
+dev.off()
 
 ################################ 2)
 NAaux <- NAm2[,-(1:7)]
@@ -54,8 +56,8 @@ caxes=c(1,2)
 plot(pcaNAm2$x[,caxes],col="white")
 i = 3 # i peut aller de 1 à npop=27
 print(names[i])
-lines(pcaNAm2$x[which(NAm2[,3]==names[i]),caxes],type="p",col=colPalette[i],pch=pch[i])
-legend("top",legend=names,col=colPalette,lty=-1,pch=pch,cex=.75,ncol=3,lwd=2)
+lines(pcaNAm2$x[which(NAm2[,3]==names[i]),caxes],type="p",col=colPalette[i],pch=pch[i], cex=0.7)
+legend("bottomleft",legend=names,col=colPalette,lty=-1,pch=pch,cex=.60,ncol=3,lwd=2)
 
 ################################ 3.c)
 summary(pcaNAm2) # donne pour tous => les deux premières composantes principales expliquent 3,57% de la variance totale
