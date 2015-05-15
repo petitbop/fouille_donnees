c1a58bf629e462dcce4167295b51318eb5aecadc
Florian Privé <florian.prive@ensimag.grenble-inp.fr>
2015-05-15 20:21:44 +0200

    55%

--- tp4/scindeur.c
+++ tp4/scindeur.c
@@ -42,34 +42,34 @@ int question1(uint16_t nb_docs[NB_CAT]) {
     return max;
 }
 
-void question2 (List **base_apprentissage, List **base_test) {
+void question2(List **base_apprentissage, List **base_test) {
     List *base_app = *base_apprentissage;
     List *base_t = *base_test;
-    
-        time_t t;
-        double r;
 
-        /* Intializes random number generator */
-        srand((unsigned) time(&t));
+    time_t t;
+    double r;
 
-        // ratio idealement voulu
-        const double ratio = (double) TAILLE_APPRENTISSAGE_VOULUE / (double) NB_TOTAL_DOCUMENTS;
-        /* on scinde aleatoire la collection suivant deux ensembles ; 
-        base d'entraînement (environ 52500 documents) et base de test (environ 18203 documents). 
-         */
-        int i;
-        for (i = NB_TOTAL_DOCUMENTS; i > 0; i--) { // on part de la fin car on ajoute en debut de chaine
-            r = r2();
-            if (r < ratio) {
-                base_app = cons(i, base_app);
-            } else {
-                base_t = cons(i, base_t);
-            }
+    /* Intializes random number generator */
+    srand((unsigned) time(&t));
+
+    // ratio idealement voulu
+    const double ratio = (double) TAILLE_APPRENTISSAGE_VOULUE / (double) NB_TOTAL_DOCUMENTS;
+    /* on scinde aleatoire la collection suivant deux ensembles ; 
+    base d'entraînement (environ 52500 documents) et base de test (environ 18203 documents). 
+     */
+    int i;
+    for (i = NB_TOTAL_DOCUMENTS; i > 0; i--) { // on part de la fin car on ajoute en debut de chaine
+        r = r2();
+        if (r < ratio) {
+            base_app = cons(i, base_app);
+        } else {
+            base_t = cons(i, base_t);
         }
-        
-        *base_apprentissage = base_app;
+    }
+
+    *base_apprentissage = base_app;
     *base_test = base_t;
-    }    
+}
 
 // Algorithm 5: Modèle multivarié de Barnoulli, phase d'apprentissage
 
@@ -89,7 +89,7 @@ void apprentissageBernoulli(List *base_app, uint16_t N[NB_CAT],
     uint32_t num_ligne = 1;
 
     fscanf(fichier, "%d", &num_cat);
-    while (!fin_de_fichier && (base_app != NULL)) {    
+    while (!fin_de_fichier && (base_app != NULL)) {
         ListeMotsClasse *prec = df[num_cat - 1];
         ListeMotsClasse *cour = prec->next;
         fin_de_ligne = 0;
@@ -134,7 +134,7 @@ int testBernoulli(List *base_test, uint16_t N[NB_CAT],
     initPiF(PiF, N, df, m);
 
     FILE* fichier = fopen("BaseReuters-29", "r");
-    int num_cat, num_mot, nb_occur;
+    int num_cat, num_mot, nb_occur, num_cat_prec;
     int fin_de_ligne = 0;
     int fin_de_fichier = 0;
     int est_test; //fait partie ou non de la base de test
@@ -154,14 +154,107 @@ int testBernoulli(List *base_test, uint16_t N[NB_CAT],
         } else {
             est_test = 0;
         }
-        
+
         // on construit la chaine des mots de la ligne
         List *motsLigne = NULL;
         while (!fin_de_ligne) {
             fscanf_value = fscanf(fichier, " %d:%d", &num_mot, &nb_occur);
-            if (fscanf_value == 2) {                
+            if (fscanf_value == 2) {
                 if (est_test) {
                     motsLigne = cons(num_mot, motsLigne);
+                }
+            } else if (fscanf_value == -1) {
+                fin_de_ligne = 1;
+                fin_de_fichier = 1;
+                num_cat_prec = num_cat;
+            } else { // on a lu la categorie de la prochaine ligne
+                fin_de_ligne = 1;
+                num_ligne++;
+                num_cat_prec = num_cat;
+                num_cat = num_mot;
+            }
+        }
+        if (est_test) { // si ce doc fait partie de la base de test
+            // on la met dans l'ordre
+            motsLigne = inverser(motsLigne);
+
+            // on estime la classe de ce document (cette ligne)
+            double max = -DBL_MAX;
+            uint8_t k_max = 0;
+            uint8_t k;
+            for (k = 0; k < NB_CAT; k++) {
+                List *mot = motsLigne;
+                double PiFk = PiF[k];
+                uint16_t Nk = N[k];
+                ListeMotsClasse *cour = df[k];
+                while (mot != NULL) {
+                    // wid = 1 ; il faut mettre à jour avec la bonne valeur
+                    uint16_t dfki = findDfki(mot->val, &cour);
+                    PiFk += log((double) (dfki + 1)) - log((double) (Nk - dfki + 1));
+                    mot = mot->next;
+                }
+                // on met à jour le max et son indice
+                if (max < PiFk) {
+                    max = PiFk;
+                    k_max = k;
+                }
+            }
+
+            // on regarde si cette estimation est bonne
+            if (k_max == (num_cat_prec - 1)) {
+                nbJuste++;
+            }
+
+            // on libere la memoire
+            motsLigne = free_list(motsLigne);
+        }
+    }
+
+    fclose(fichier);
+
+    assert(base_test == NULL);
+
+    return nbJuste;
+}
+
+// Algorithm 7: Modèle multinomial, phase d'apprentissage
+
+void apprentissageMultinomial(List *base_app, uint16_t N[NB_CAT],
+        uint32_t D[NB_CAT], ListeMotsClasse * tf[NB_CAT]) {
+
+    // on initialise les chaines de df avec un element de debut et de fin
+    // on initialise également les N[k] à 0
+    init3(N, D, tf);
+
+    FILE* fichier = fopen("BaseReuters-29", "r");
+    int num_cat, num_mot, nb_occur;
+    int fin_de_ligne = 0;
+    int fin_de_fichier = 0;
+    int est_app; //fait partie ou non de la base d'apprentissage
+    int fscanf_value;
+    uint32_t num_ligne = 1;
+
+    fscanf(fichier, "%d", &num_cat);
+    while (!fin_de_fichier && (base_app != NULL)) {
+        ListeMotsClasse *prec = tf[num_cat - 1];
+        ListeMotsClasse *cour = prec->next;
+        fin_de_ligne = 0;
+        if (num_ligne == base_app->val) {
+            est_app = 1;
+            List *cell = base_app;
+            base_app = base_app->next; // on avance dans la base d'apprentissage
+            free(cell);
+            N[num_cat - 1]++;
+        } else {
+            est_app = 0;
+        }
+
+        while (!fin_de_ligne) {
+            fscanf_value = fscanf(fichier, " %d:%d", &num_mot, &nb_occur);
+            if (fscanf_value == 2) {
+                if (est_app) {
+                    insererDans2(&prec, &cour, num_mot, nb_occur);
+                    D[num_cat - 1] += nb_occur;
                 }
             } else if (fscanf_value == -1) {
                 fin_de_ligne = 1;
@@ -172,47 +265,105 @@ int testBernoulli(List *base_test, uint16_t N[NB_CAT],
                 num_cat = num_mot;
             }
         }
+    }
+
+    fclose(fichier);
+    assert(base_app == NULL);
+}
+
+// Algorithm 8: Modèle multinomial, phase de test
+
+int testMultinomial(List *base_test, uint16_t N[NB_CAT], uint32_t D[NB_CAT],
+        ListeMotsClasse * tf[NB_CAT], int m) {
+
+    // on initialise les PiF[k]
+    double PiF[NB_CAT];
+    int k;
+    for (k = 0; k < NB_CAT; k++) {
+        PiF[k] = log((double) N[k] / (double) m);
+    }
+
+    FILE* fichier = fopen("BaseReuters-29", "r");
+    int num_cat, num_mot, nb_occur, num_cat_prec;
+    int fin_de_ligne = 0;
+    int fin_de_fichier = 0;
+    int est_test; //fait partie ou non de la base de test
+    int fscanf_value;
+    uint32_t num_ligne = 1;
+    int nbJuste = 0;
+
+    fscanf(fichier, "%d", &num_cat);
+    while (!fin_de_fichier && (base_test != NULL)) {
+        printf("Ligne %d\n", num_ligne); //DEBUG
+        fin_de_ligne = 0;
+        if (num_ligne == base_test->val) {
+            est_test = 1;
+            List *cell = base_test;
+            base_test = base_test->next; // on avance dans la base de test
+            free(cell);
+        } else {
+            est_test = 0;
+        }
+
+        // on construit la chaine des mots de la ligne
+        List *motsLigne = NULL;
+        while (!fin_de_ligne) {
+            fscanf_value = fscanf(fichier, " %d:%d", &num_mot, &nb_occur);
+            if (fscanf_value == 2) {
+                if (est_test) {
+                    motsLigne = cons(num_mot, motsLigne);
+                }
+            } else if (fscanf_value == -1) {
+                fin_de_ligne = 1;
+                fin_de_fichier = 1;
+                num_cat_prec = num_cat;
+            } else { // on a lu la categorie de la prochaine ligne
+                fin_de_ligne = 1;
+                num_ligne++;
+                num_cat_prec = num_cat;
+                num_cat = num_mot;
+            }
+        }
         if (est_test) { // si ce doc fait partie de la base de test
             // on la met dans l'ordre
             motsLigne = inverser(motsLigne);
-            
+
             // on estime la classe de ce document (cette ligne)
             double max = -DBL_MAX;
             uint8_t k_max = 0;
             uint8_t k;
             for (k = 0; k < NB_CAT; k++) {
                 List *mot = motsLigne;
-                double PiFk = PiF[k]; 
-                uint16_t Nk = N[k];
-                ListeMotsClasse *cour = df[k];
-                //int taille_dfk = print2(df[k]); //DEBUG
-                while (mot != NULL) { 
+                double PiFk = PiF[k];
+                uint16_t Dk = D[k];
+                ListeMotsClasse *cour = tf[k];
+                while (mot != NULL) {
                     // wid = 1 ; il faut mettre à jour avec la bonne valeur
-                    uint16_t dfki = findDfki(mot->val, &cour);
-                    PiFk += log( (double)(dfki + 1) / (double)(Nk - dfki + 1) );
+                    uint16_t tfki = findDfki(mot->val, &cour);
+                    PiFk += log((double) (tfki + 1)) - log((double) (Dk + taille_voca));
                     mot = mot->next;
                 }
                 // on met à jour le max et son indice
                 if (max < PiFk) {
                     max = PiFk;
                     k_max = k;
-                }            
+                }
             }
-            
+
             // on regarde si cette estimation est bonne
-            if (k_max == num_cat-1) {
+            if (k_max == (num_cat_prec - 1)) {
                 nbJuste++;
-            }   
-            
+            }
+
             // on libere la memoire
-            free_list(motsLigne);
+            motsLigne = free_list(motsLigne);
         }
     }
 
     fclose(fichier);
-    
+
     assert(base_test == NULL);
-    
+
     return nbJuste;
 }
 
@@ -233,43 +384,80 @@ int main() {
     }
     assert(somme_verif == NB_TOTAL_DOCUMENTS);
 
-    
-    
+
+
     int nb;
     for (nb = 0; nb < 1; nb++) {
+
+        /* Question 2 */
+
+        printf("\nQuestion 2 :\n");
+        List *base_apprentissage = NULL;
+        List *base_test = NULL;
+        question2(&base_apprentissage, &base_test);
+
+        printf("Base d'apprentissage :\n");
+        int m_app = print(base_apprentissage);
+        printf("Base de test :\n");
+        int m_test = print(base_test);
+
+
+        /* Question 3 */
+
+        printf("\nQuestion 3 :\n");
+        uint16_t N[NB_CAT];
+        ListeMotsClasse * df[NB_CAT];
+
+        apprentissageBernoulli(base_apprentissage, N, df);
+
+
+        /* Question 4 */
+
+        printf("\nQuestion 4 :\n");
+        int nbJuste = testBernoulli(base_test, N, df, m_app);
+        printf("Dans la base de test du modèle multivarié de Bernoulli,\n");
+        printf("Le taux de bonne classification est de %f\n", (double) (100 * nbJuste) / (double) m_test);
+
+
+        // on free df
+        free_tab(df);
+    }
     
-    /* Question 2 */
+    for (nb = 0; nb < 1; nb++) {
 
-    printf("\nQuestion 2 :\n");
-    List *base_apprentissage = NULL;
-    List *base_test = NULL;
-    question2(&base_apprentissage, &base_test);
-    
-    printf("Base d'apprentissage :\n");
-    int m_app = print(base_apprentissage);
-    printf("Base de test :\n");
-    int m_test = print(base_test);
+        /* Question 2 */
 
+        printf("\nQuestion 2 :\n");
+        List *base_apprentissage = NULL;
+        List *base_test = NULL;
+        question2(&base_apprentissage, &base_test);
 
-    /* Question 3 */
-
-    printf("\nQuestion 3 :\n");
-    uint16_t N[NB_CAT];
-    ListeMotsClasse * df[NB_CAT];
-
-    apprentissageBernoulli(base_apprentissage, N, df);
-    
-    
-    /* Question 4 */
-
-    printf("\nQuestion 4 :\n");
-    int nbJuste = testBernoulli(base_test, N, df, m_app);
-    printf("Dans la base de test du modèle multivarié de Bernoulli,\n");
-    printf("Le taux de bonne classification est de %f\n", (double)(100*nbJuste) / (double)m_test);
+        printf("Base d'apprentissage :\n");
+        int m_app = print(base_apprentissage);
+        printf("Base de test :\n");
+        int m_test = print(base_test);
 
 
-    // on free df
-    free_tab(df);
+        /* Question 3 */
+
+        printf("\nQuestion 3 :\n");
+        uint16_t N[NB_CAT];
+        uint32_t D[NB_CAT];
+        ListeMotsClasse * tf[NB_CAT];
+
+        apprentissageMultinomial(base_apprentissage, N, D, tf);
+
+
+        /* Question 4 */
+
+        printf("\nQuestion 4 :\n");
+        int nbJuste = testMultinomial(base_test, N, D, tf, m_app);
+        printf("Dans la base de test du modèle multinomial,\n");
+        printf("Le taux de bonne classification est de %f\n", (double) (100 * nbJuste) / (double) m_test);
+
+
+        // on free tf
+        free_tab(tf);
     }
 
 
